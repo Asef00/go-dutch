d0d2122f8b295d76ce8c2873dd173c2f47954c8c
Giga <asefhosseini00@gmail.com>
2023-12-08 00:40:05 +0330

    create event type

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-const events = []
+interface Event {
+  id: number
+  name: string
+  people: string[]
+  color: string
+}
+
+const events: Event[] = [{ id: 1, name: 'Trip', people: ['Asef', 'Feri'], color: 'green' }]
 </script>
 
 <template>
   <main>
     <h1>Events</h1>
     <div v-if="!events.length">...No Events...</div>
-    <div v-else v-for="event in events" :key="event">{{ event }}</div>
+    <div v-else v-for="event in events" :key="event.id">{{ event }}</div>
   </main>
 </template>
