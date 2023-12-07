950651d91492086e2066373ba7af3dd512550735
Giga <asefhosseini00@gmail.com>
2023-12-08 00:31:42 +0330

    create basic event showing structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+const events = []
 </script>
 
 <template>
   <main>
-    Hello World
+    <h1>Events</h1>
+    <div v-if="!events.length">...No Events...</div>
+    <div v-else v-for="event in events" :key="event">{{ event }}</div>
   </main>
 </template>
