e9162c386bedb8a2d1619b38f3071696f32c3884
Giga <asefhosseini00@gmail.com>
2023-12-08 10:33:45 +0330

    create a general input component

---
+++ src/components/VInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <label>
+    {{ props.label }}
+
+    <input
+      :type="props.type"
+      :value="modelValue"
+      :placeholder="props.placeholder"
+      @input="updateValue"
+    />
+  </label>
+</template>
+
+<script setup lang="ts">
+interface InputProps {
+  label: string
+  type: 'text' | 'number' | 'password'
+  modelValue: string | number
+  placeholder: string
+}
+
+const props = defineProps<InputProps>()
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (event: any) => {
+  emit('update:modelValue', event.target.value)
+}
+</script>
