014195bae3d25e43f7729278808b2374f3fcd75f
Giga <asefhosseini00@gmail.com>
2023-12-08 12:11:21 +0330

    create eventView and go-back functionality

---
+++ src/views/EventView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="blog-post-details">
+    <h1>{{ id }}</h1>
+    <button @click="goBack">Go Back</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const id = router.currentRoute.value.params.id
+
+const goBack = () => {
+  router.back()
+}
+</script>
