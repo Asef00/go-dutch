27f3e3bca8588fa05b6e0f50c13ebfed5411177e
Giga <asefhosseini00@gmail.com>
2023-12-08 12:31:59 +0330

    create a general loading component

---
+++ src/components/VLoading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="loading">
+    <span class="icon-spinner9"></span>
+  </div>
+</template>
+
+<style>
+.loading {
+  animation: spin 1s linear infinite;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
